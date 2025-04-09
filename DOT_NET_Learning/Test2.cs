b9aada42a87a1a3e66981ae105be88a2559bbd99
rupalibichukale13 <160692463+rupalibichukale13@users.noreply.github.com>
2025-04-09 08:32:53 +0530

    Added file by RupaliBichukale

---
+++ DOT_NET_Learning/Test2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOT_NET_Learning
+{
+    internal class Test2
+    {
+    }
+}
